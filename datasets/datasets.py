192c8712c3dba8ff0ad82b8d14d638c0ab24afbd
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2023-06-04 15:12:27 -0700

    Added more for torch

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,10 +1,15 @@
-from torchvision import datasets
+from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
-def numbers_dataset(location='./data', download=True, normalize=False, Flatten=False):
+def numbers_dataset(location='./data', download=True, normalize=False, Torch=False):
     train_set = datasets.MNIST(location, train=True, download=download)
     test_set = datasets.MNIST(location, train=False, download=download)
     
+    if Torch:
+        train_data = datasets.MNIST(location, train=True, download=download, transform=transforms.ToTensor())
+        test_data = datasets.MNIST(location, train=False, download=download, transform=transforms.ToTensor())
+        return train_data, test_data
+
     if normalize:
         train_set_array = train_set.data.numpy()/255
         train_set_labels = train_set.targets.numpy()/255
@@ -18,6 +23,7 @@ def numbers_dataset(location='./data', download=True, normalize=False, Flatten=F
         test_set_labels = test_set.targets.numpy()
 
     return train_set_array, test_set_array, train_set_labels, test_set_labels
+
 
 def visualize(dataset, label, image_num):
     mnist_image = dataset[image_num].reshape(28, 28)
